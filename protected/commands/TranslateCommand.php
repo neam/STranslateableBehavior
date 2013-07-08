9726621c8387b2cb873a81abaf2beb7615dcffcc
Fredrik Wollsén <fredrik@neam.se>
2013-07-08 15:39:18 +0200

    Formatting according to yii framework coding standards

--- protected/commands/TranslateCommand.php
+++ protected/commands/TranslateCommand.php
@@ -7,210 +7,209 @@
 class TranslateCommand extends CConsoleCommand
 {
 
-    /**
-     * @var array
-     */
-    public $models = array();
+	/**
+	 * @var array
+	 */
+	public $models = array();
 
-    /**
-     * @var null
-     */
-    public $langs = null;
+	/**
+	 * @var null
+	 */
+	public $langs = null;
 
-    /**
-     * Source language
-     *
-     * @var string
-     */
-    public $sourceLang;
+	/**
+	 * Source language
+	 *
+	 * @var string
+	 */
+	public $sourceLang;
 
-    /**
-     * @var array
-     */
-    public $up = array();
+	/**
+	 * @var array
+	 */
+	public $up = array();
 
-    /**
-     * @var array
-     */
-    public $down = array();
+	/**
+	 * @var array
+	 */
+	public $down = array();
 
-    /**
-     * If we should be verbose
-     *
-     * @var bool
-     */
-    private $_verbose = false;
+	/**
+	 * If we should be verbose
+	 *
+	 * @var bool
+	 */
+	private $_verbose = false;
 
-    /**
-     * Array of columns to create
-     *
-     * @var array
-     */
-    public $columns = array();
+	/**
+	 * Array of columns to create
+	 *
+	 * @var array
+	 */
+	public $columns = array();
 
-    /**
-     * Write a string to standard output if we're verbose
-     *
-     * @param $string
-     */
-    public function d($string)
-    {
-        if ($this->_verbose) {
-            print $string;
-        }
-    }
+	/**
+	 * Write a string to standard output if we're verbose
+	 *
+	 * @param $string
+	 */
+	public function d($string)
+	{
+		if ($this->_verbose) {
+			print $string;
+		}
+	}
 
-    /**
-     * Execute the command
-     *
-     * @param array $args
-     * @return bool|int
-     */
-    public function run($args)
-    {
-        if (in_array('--verbose', $args)) {
-            $this->_verbose = true;
-        }
-        $this->d("\033[37mLoading languages\n");
-        $this->_loadLanguages();
+	/**
+	 * Execute the command
+	 *
+	 * @param array $args
+	 * @return bool|int
+	 */
+	public function run($args)
+	{
+		if (in_array('--verbose', $args)) {
+			$this->_verbose = true;
+		}
+		$this->d("\033[37mLoading languages\n");
+		$this->_loadLanguages();
 
-		$this->models = $this->getModels();
+		$this->models = $this->_getModels();
 
-        if (sizeof($this->models) > 0) {
-            $this->_createMigration();
-            } else {
+		if (sizeof($this->models) > 0) {
+			$this->_createMigration();
+		} else {
 			echo "Found no models with a translate() method";
-        }
-    }
+		}
+	}
 
-    /**
-     * Create the migration files
-     */
-    protected function _createMigration()
-    {
-        $this->d("Creating the migration...\n");
-        foreach ($this->models as $modelName => $modelClass) {
-            $this->d("\t...$modelName: ");
-            foreach ($this->langs as $lang) {
-                $this->d($lang);
-                $this->_processLang($lang, $modelClass);
-            }
-            $this->d("\n");
-        }
+	/**
+	 * Create the migration files
+	 */
+	private function _createMigration()
+	{
+		$this->d("Creating the migration...\n");
+		foreach ($this->models as $modelName => $modelClass) {
+			$this->d("\t...$modelName: ");
+			foreach ($this->langs as $lang) {
+				$this->d($lang);
+				$this->_processLang($lang, $modelClass);
+			}
+			$this->d("\n");
+		}
 
-        $this->_createMigrationFile();
-    }
+		$this->_createMigrationFile();
+	}
 
-    /**
-     * @param $lang
-     * @param $model
-     */
-    protected function _processLang($lang, $model)
-    {
-        foreach ($model->translate() as $attribute) {
-            $newName = $attribute . '_' . $lang;
-            if (!isset($model->metaData->columns[$newName])
-                && $this->_checkColumnExists($model, $attribute)) {
-                // Rename columns back and forth
-                if ($lang == $this->sourceLang) {
-                    $this->d("Rename $attribute to $newName\n");
-                    $this->up[] = '$this->renameColumn(\''. $model->tableName() . '\', \'' . $attribute
-                        . '\', \'' . $newName . '\');';
-                    $this->down[] = '$this->renameColumn(\'' . $model->tableName() . '\', \''
-                        . $newName . '\', \'' . $attribute . '\');';
-                } else {
-                    $this->up[] = '$this->addColumn(\''. $model->tableName() . '\', \'' . $newName
-                        . '\', \'' . $this->_getColumnDbType($model, $attribute) . '\');';
-                    $this->down[] = '$this->dropColumn(\'' . $model->tableName() . '\', \''
-                        . $newName . '\');';
-                }
-            }
-        }
-    }
+	/**
+	 * @param $lang
+	 * @param $model
+	 */
+	private function _processLang($lang, $model)
+	{
+		foreach ($model->translate() as $attribute) {
+			$newName = $attribute . '_' . $lang;
+			if (!isset($model->metaData->columns[$newName]) && $this->_checkColumnExists($model, $attribute)) {
+				// Rename columns back and forth
+				if ($lang == $this->sourceLang) {
+					$this->d("Rename $attribute to $newName\n");
+					$this->up[] = '$this->renameColumn(\'' . $model->tableName() . '\', \'' . $attribute
+						. '\', \'' . $newName . '\');';
+					$this->down[] = '$this->renameColumn(\'' . $model->tableName() . '\', \''
+						. $newName . '\', \'' . $attribute . '\');';
+				} else {
+					$this->up[] = '$this->addColumn(\'' . $model->tableName() . '\', \'' . $newName
+						. '\', \'' . $this->_getColumnDbType($model, $attribute) . '\');';
+					$this->down[] = '$this->dropColumn(\'' . $model->tableName() . '\', \''
+						. $newName . '\');';
+				}
+			}
+		}
+	}
 
-    /**
-     * @param $model
-     * @param $column
-     * @return bool
-     */
-    protected function _checkColumnExists($model, $column)
-    {
-        return isset($model->metaData->columns[$column]);
-    }
+	/**
+	 * @param $model
+	 * @param $column
+	 * @return bool
+	 */
+	private function _checkColumnExists($model, $column)
+	{
+		return isset($model->metaData->columns[$column]);
+	}
 
-    /**
-     * @param $model
-     * @param $column
-     * @return string
-     */
-    protected function _getColumnDbType($model, $column)
-    {
-        $data = $model->metaData->columns[$column];
-        $isNull = $data->allowNull ? "null" : "not null";
+	/**
+	 * @param $model
+	 * @param $column
+	 * @return string
+	 */
+	private function _getColumnDbType($model, $column)
+	{
+		$data = $model->metaData->columns[$column];
+		$isNull = $data->allowNull ? "null" : "not null";
 
-        return $data->dbType . ' ' . $isNull;
-    }
+		return $data->dbType . ' ' . $isNull;
+	}
 
-    /**
-     * Load model file and create new instance
-     *
-     * @param mixed $path Path to model
-     * @access protected
-     */
-    protected function _loadModelFile($path)
-    {
-        $this->d("Loading model from $path\n");
-        $class_name = str_replace('.php', '', basename($path));
-        if (!class_exists($class_name, false)) {
-            include($path);
-            $model = new $class_name();
-            if (method_exists($model, 'translate')) {
-                $this->models[$class_name] = $model;
-            }
-        }
-    }
+	/**
+	 * Load model file and create new instance
+	 *
+	 * @param mixed $path Path to model
+	 * @access protected
+	 */
+	private function _loadModelFile($path)
+	{
+		$this->d("Loading model from $path\n");
+		$className = str_replace('.php', '', basename($path));
+		if (!class_exists($className, false)) {
+			include($path);
+			$model = new $className();
+			if (method_exists($model, 'translate')) {
+				$this->models[$className] = $model;
+			}
+		}
+	}
 
-    /**
-     * Load languages from main config.
-     *
-     * @access protected
-     */
-    protected function _loadLanguages()
-    {
-        // Load main.php config file
-        $file = realpath(dirname(__FILE__) . '/../config/main.php');
-        if (!file_exists($file)) {
-            print("Config not found\n");
-            exit("Error loading config file main.php.\n");
-        } else {
-            $config = require($file);
-            $this->d("Config loaded\n");
-        }
+	/**
+	 * Load languages from main config.
+	 *
+	 * @access protected
+	 */
+	private function _loadLanguages()
+	{
+		// Load main.php config file
+		$file = realpath(dirname(__FILE__) . '/../config/main.php');
+		if (!file_exists($file)) {
+			print("Config not found\n");
+			exit("Error loading config file main.php.\n");
+		} else {
+			$config = require($file);
+			$this->d("Config loaded\n");
+		}
 
-        if (!isset($config['params']['languages'])) {
-            exit("Please, define possible languages in config file.\n");
-        }
+		if (!isset($config['params']['languages'])) {
+			exit("Please, define possible languages in config file.\n");
+		}
 
-        if (!isset($config['sourceLanguage'])) {
-            exit("Please, define the source language in config file.\n");
-        }
+		if (!isset($config['sourceLanguage'])) {
+			exit("Please, define the source language in config file.\n");
+		}
 
-        $this->langs = $config['params']['languages'];
-        $this->sourceLang = $config['sourceLanguage'];
-    }
+		$this->langs = $config['params']['languages'];
+		$this->sourceLang = $config['sourceLanguage'];
+	}
 
-    /**
-     * Create migration file
-     */
-    protected function _createMigrationFile()
-    {
-        if (count($this->up) == 0) {
-            exit("Database up to date\n");
-        }
+	/**
+	 * Create migration file
+	 */
+	private function _createMigrationFile()
+	{
+		if (count($this->up) == 0) {
+			exit("Database up to date\n");
+		}
 
-        $migrationName = 'm' . gmdate('ymd_His') . '_translate';
+		$migrationName = 'm' . gmdate('ymd_His') . '_translate';
 
-        $phpCode = '<?php
+		$phpCode = '<?php
 class ' . $migrationName . ' extends CDbMigration
 {
     public function up()
@@ -224,40 +223,43 @@ class ' . $migrationName . ' extends CDbMigration
     }
 }' . "\n";
 
-        $migrationsDir = realpath(Yii::app()->basePath.'/migrations');
-        if (!realpath($migrationsDir)) {
-            die(sprintf('Please create migration directory %s first', $migrationsDir));
-        }
+		$migrationsDir = realpath(Yii::app()->basePath . '/migrations');
+		if (!realpath($migrationsDir)) {
+			die(sprintf('Please create migration directory %s first', $migrationsDir));
+		}
 
-        $migrationFile = $migrationsDir . '/' . $migrationName . '.php';
-        $f = fopen($migrationFile, 'w') or die("Can't open file");
-        fwrite($f, $phpCode);
-        fclose($f);
+		$migrationFile = $migrationsDir . '/' . $migrationName . '.php';
+		$f = fopen($migrationFile, 'w') or die("Can't open file");
+		fwrite($f, $phpCode);
+		fclose($f);
 
-        print "Migration successfully created.\n";
-        print "See $migrationName\n";
-        print "To apply migration enter: ./yiic migrate\n";
-    }
+		print "Migration successfully created.\n";
+		print "See $migrationName\n";
+		print "To apply migration enter: ./yiic migrate\n";
+	}
 
 	// Originally from gii-template-collection / fullCrud / FullCrudGenerator.php
-	protected function getModels() {
+	private function _getModels()
+	{
 		$models = array();
 		$aliases = array();
 		$aliases[] = 'application.models';
 		foreach (Yii::app()->getModules() as $moduleName => $config) {
-			if($moduleName != 'gii')
+			if ($moduleName != 'gii')
 				$aliases[] = $moduleName . ".models";
 		}
 
 		foreach ($aliases as $alias) {
-			if (!is_dir(Yii::getPathOfAlias($alias))) continue;
+			if (!is_dir(Yii::getPathOfAlias($alias)))
+				continue;
 			$files = scandir(Yii::getPathOfAlias($alias));
-			Yii::import($alias.".*");
+			Yii::import($alias . ".*");
 			foreach ($files as $file) {
-				if ($fileClassName = $this->checkFile($file, $alias)) {
-						$classname = sprintf('%s.%s',$alias,$fileClassName);
-						Yii::import($classname);
-					try {
+				if ($fileClassName = $this->_checkFile($file, $alias)) {
+					$classname = sprintf('%s.%s', $alias, $fileClassName);
+					Yii::import($classname);
+					try
+					{
 						$model = @new $fileClassName;
 						if (method_exists($model, 'translate')) {
 							if (method_exists($model, 'behaviors')) {
@@ -267,11 +269,14 @@ class ' . $migrationName . ' extends CDbMigration
 								}
 							}
 						}
-					} catch (ErrorException $e) {
+					} catch (ErrorException $e)
+					{
 						break;
-					} catch (CDbException $e) {
+					} catch (CDbException $e)
+					{
 						break;
-					} catch (Exception $e) {
+					} catch (Exception $e)
+					{
 						break;
 					}
 				}
@@ -282,22 +287,19 @@ class ' . $migrationName . ' extends CDbMigration
 	}
 
 	// Imported from gii-template-collection / fullCrud / FullCrudGenerator.php
-	private function checkFile($file, $alias = '') {
-		if (substr($file, 0, 1) !== '.'
-				&& substr($file, 0, 2) !== '..'
-				&& substr($file, 0, 4) !== 'Base'
-			&& $file != 'GActiveRecord'
-			&& strtolower(substr($file, -4)) === '.php') {
+	private function _checkFile($file, $alias = '')
+	{
+		if (substr($file, 0, 1) !== '.' && substr($file, 0, 2) !== '..' && substr($file, 0, 4) !== 'Base' && $file != 'GActiveRecord' && strtolower(substr($file, -4)) === '.php') {
 			$fileClassName = substr($file, 0, strpos($file, '.'));
-			if (class_exists($fileClassName)
-					&& is_subclass_of($fileClassName, 'CActiveRecord')) {
+			if (class_exists($fileClassName) && is_subclass_of($fileClassName, 'CActiveRecord')) {
 				$fileClass = new ReflectionClass($fileClassName);
-				if ($fileClass->isAbstract())
+				if ($fileClass->isAbstract()) {
 					return null;
-				else
+				} else {
 					return $models[] = $fileClassName;
+				}
 			}
 		}
 	}
-	
+
 }
--- protected/extensions/STranslateableBehavior/STranslateableBehavior.php
+++ protected/extensions/STranslateableBehavior/STranslateableBehavior.php
@@ -9,26 +9,25 @@
  * @copyright firstrow@gmail.com
  * @author Andrej Bubis' <firstrow@gmail.com> 
  */
-class STranslateableBehavior extends CActiveRecordBehavior {
+class STranslateableBehavior extends CActiveRecordBehavior
+{
 
-    /**
-     * Apply translated attributes to current lang 
-     * 
-     * @access public
-     */
-    public function afterFind()
-    {
-        $forTranslate = $this->owner->translate();
-               
-		if (sizeof($forTranslate) > 0)
-		{
-			foreach($forTranslate as $attr)
-			{
-				$attrName = $attr.'_'.Yii::app()->language;
+	/**
+	 * Apply translated attributes to current lang 
+	 * 
+	 * @access public
+	 */
+	public function afterFind()
+	{
+		$forTranslate = $this->owner->translate();
+
+		if (sizeof($forTranslate) > 0) {
+			foreach ($forTranslate as $attr) {
+				$attrName = $attr . '_' . Yii::app()->language;
 				if (array_key_exists($attrName, $this->owner->attributes))
 					$this->owner->$attr = $this->owner->$attrName;
 			}
 		}
-    }
+	}
 
 }
